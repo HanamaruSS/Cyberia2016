bdcf72eeaab601d241d973a92e26bc96f626670a
HanamaruSS <indra.rg@gmail.com>
2016-10-20 06:35:45 +0800

    ALMOST DONEgit status! add content for faq, past. edit css for faq, past. change timer to reg closing date

--- js/flipclock-time.js
+++ js/flipclock-time.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	var currentDate = new Date();
 
 	// Set the date of start
-	var futureDate  = new Date(2016, 11, 5);
+	var futureDate  = new Date(2016, 10, 9, 23, 59);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
